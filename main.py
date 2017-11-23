a93b9dcea980789abc8f4a58e45f2ac5b3b70560
xealml <935547148@qq.com>
2017-11-23 18:14:56 +0800

    11.23

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import HousingData.get_ziroom2 as ziroom
 import HousingData.get_woaiwojia2 as woaiwojia
 
-# ziroom.get_and_save()
+
+print('ziroom')
+ziroom.get_and_save()
+print('woaiwojia')
 woaiwojia.get_and_save()
